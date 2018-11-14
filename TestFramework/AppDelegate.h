2549c42f8a58b290b189645449582f98e7b7701d
obarong <dustinljr@gmail.com>
2018-11-14 13:24:04 +0800

    Initial Commit

---
+++ TestFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestFramework
+//
+//  Created by 罗锦荣 on 2018/11/14.
+//  Copyright © 2018年 obarong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestFramework
+//
+//  Created by 罗锦荣 on 2018/11/14.
+//  Copyright © 2018年 obarong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
